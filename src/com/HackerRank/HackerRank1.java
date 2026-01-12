93e236fafd666d0646a78c3b062fd2fc4882b7be
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-12 22:52:26 +0530

    Program to convert Integer to String value and vice cersa

--- src/com/HackerRank/HackerRank1.java
+++ src/com/HackerRank/HackerRank1.java
@@ -11,22 +11,26 @@ n can range between -100 to 100  inclusive.
  */
 public class HackerRank1 {
 public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
-	String s = null;
-	System.out.println("value of n: "+n);
-	
-	//convert n to a string now, if n ranges between -100 and 100
-	if(n>=-100 && n<=100)
-	{
-		s=String.valueOf(n);
-		//The above line converts Integer 25 to String "25"
-		System.out.println(s);
+	try (Scanner sc = new Scanner(System.in)) {
+		int n=sc.nextInt();
+		String s = null;
+		System.out.println("value of n: "+n);
+		
+		//convert n to a string now, if n ranges between -100 and 100
+		if(n>=-100 && n<=100)
+		{
+			s=String.valueOf(n);
+			//The above line converts Integer 25 to String "25"
+			System.out.println(s);
+		}
+		if(n==Integer.parseInt(s))
+			//Integer.parseInt converts String "25" to Integer 25
+			System.out.println("Good job");
+		else
+			System.out.println("Wrong Answer");
+	} catch (NumberFormatException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	if(n==Integer.parseInt(s))
-		//Integer.parseInt converts String "25" to Integer 25
-		System.out.println("Good job");
-	else
-		System.out.println("Wrong Answer");
 }
 }
