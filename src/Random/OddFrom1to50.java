77329c8a535455c0b73a8128d44b483892b1e3c7
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-13 19:53:55 +0530

    odd numbers from 1 to 50

---
+++ src/Random/OddFrom1to50.java
@@ -0,0 +1,14 @@
+package Random;
+
+
+//Print all odd numbers from 1 to 50.
+public class OddFrom1to50 {
+public static void main(String[] args) {
+	System.out.println("odd numbers are: ");
+	for(int i=1;i<=50;i++)
+	{
+		if(i%2!=0)
+		System.out.print(i+" ");
+	}
+}
+}
