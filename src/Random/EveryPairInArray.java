bdcec3e9ecccf85eaa0668818047e76cde48f437
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-15 21:25:40 +0530

    Every pair possible in an array

---
+++ src/Random/EveryPairInArray.java
@@ -0,0 +1,16 @@
+package Random;
+
+
+public class EveryPairInArray {
+public static void main(String[] args) {
+	int arr[]= {1,2,3};
+	for(int i=0;i<arr.length-1;i++)
+	{
+		System.out.println("pair when array element is: "+arr[i]);
+		for(int j=i+1;j<arr.length;j++)
+		{
+			System.out.println("("+arr[i]+","+arr[j]+")");
+		}
+	}
+}
+}
