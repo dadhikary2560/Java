e14bbaad3f47993c38388ffd346dd7ec0eb76664
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-17 21:16:38 +0530

    .class files are added

---
+++ src/Random/PascalTriangle.java
@@ -0,0 +1,16 @@
+package Random;
+
+
+//Print a Pascal’s Triangle up to n rows.
+public class PascalTriangle {
+public static void main(String[] args) {
+	for(int lines=1;lines<=5;lines++)
+	{
+		for(int space=5;space>1;space--)
+		{
+			System.out.print(" ");
+		}
+		
+	}
+}
+}
