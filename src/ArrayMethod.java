f076e7cc6c04aa623e5b040c7fb2688dda92ee81
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-26 17:42:06 +0530

    Method to receive array as parameter

---
+++ src/ArrayMethod.java
@@ -0,0 +1,24 @@
+
+public class ArrayMethod {
+	
+	public void array1(int a[]) {
+	for(int i=0;i<a.length;i++)
+	{
+		System.out.println(a[i]);
+	}
+
+	
+	}
+	
+
+	
+	
+	
+}
+//accees modifier | Return Type | method name (List of Arguments)
+//body
+
+//static	className.method
+//instance	object.method_name
+
+
