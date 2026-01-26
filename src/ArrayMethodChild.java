5af28199d873b1d879b0d3e5370a9a7fe718e5a7
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-26 17:42:50 +0530

    Array Method Child class to call method

---
+++ src/ArrayMethodChild.java
@@ -0,0 +1,8 @@
+
+public class ArrayMethodChild extends ArrayMethod{
+public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5};
+		ArrayMethodChild obj=new ArrayMethodChild();
+		obj.array1(arr);
+}
+}
