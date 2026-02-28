151a6d8211d751ff73429cf8840821d8c136125f
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-01 00:52:00 +0530

    static constructor can not be formed

--- src/Constructor/StaticConstructor.java
+++ src/Constructor/StaticConstructor.java
@@ -7,17 +7,19 @@ public class StaticConstructor {
 	
 	private void m1() {
 		//StaticConstructor();
+		System.out.println("method m1");
 	}
 	public static void main(String[] args) {
 		System.out.println(10);			//prints 10
 		System.out.println("10"+5);		//prints 105 as 10 is a string
+		StaticConstructor obj=new StaticConstructor(); //no matter what, to call constructor
+													  //new is needed
+		obj.m1();
 	}
 }
 
 /*
  * Constructors are called only when creating an object
- * 
  * They cannot be called like regular methods
- * 
  * They must be invoked using new
  */
